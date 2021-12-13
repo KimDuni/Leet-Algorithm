962e4a6ad3706e4a0aa33fc3dfc55109f75c26ff
KimDuni <7577ksj@gmail.com>
2021-12-13 17:27:27 +0900

    Update valid-parentheses.swift
    Time: 4 ms (66.21%), Space: 14.3 MB (36.90%) - LeetHub

--- valid-parentheses/valid-parentheses.swift
+++ valid-parentheses/valid-parentheses.swift
@@ -1,16 +1,42 @@
 class Solution {
     func isValid(_ s: String) -> Bool {
-        var s = s
-        while true {
-            if s.contains("[]") || s.contains("()") || s.contains("{}") {
-                s = s.replacingOccurrences(of: "[]", with: "")
-                s = s.replacingOccurrences(of: "()", with: "")
-                s = s.replacingOccurrences(of: "{}", with: "")
-            } else {
-                break
+        var stack: [String] = []
+        
+        for c in s {
+            stack.append(String(c))
+            
+            let stackCnt = stack.count
+            if stackCnt >= 2 {
+                let last = stack.last!
+                if last == ")" || last == "}" || last == "]"  {
+                    if last.isMirror(s: stack[stack.index(before: stackCnt - 1)]) {
+                        stack.removeLast()
+                        stack.removeLast()
+                    } else {
+                        break
+                    }
+                }
             }
         }
         
-        return s.count == 0
+        return stack.count == 0
+    }
+}
+
+extension String {
+
+    func isMirror(s: String) -> Bool{
+        if s == "[" {
+            return self == "]"
+        }
+        
+        if s == "(" {
+            return self == ")"
+        }
+        
+        if s == "{" {
+            return self == "}"
+        }
+        return false
     }
 }
